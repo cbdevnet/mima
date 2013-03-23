dc2e9cc302dfefa5fc52c9e9dd1e799a739b12e6
cb@cbcdn.com <=>
2013-03-23 10:24:53 +0100

    Current project status

---
+++ mimasim/mimasim.c
@@ -0,0 +1,589 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <malloc.h>
+
+#ifndef _WIN32
+	#include <termios.h>
+	#include <unistd.h>
+
+	//this emulates conio.h's getch().
+	//via http://cboard.cprogramming.com/faq-board/27714-faq-there-getch-conio-equivalent-linux-unix.html
+	
+	int _getch(void){
+		struct termios canon,raw;
+		int ch;
+		tcgetattr(STDIN_FILENO,&canon);
+		raw=canon;
+		raw.c_lflag&=~(ICANON|ECHO);
+		tcsetattr(STDIN_FILENO,TCSANOW,&raw);
+		ch=getchar();
+		tcsetattr(STDIN_FILENO,TCSANOW,&canon);
+		return ch;
+	}
+
+	#define _GETCH_ _getch
+#else
+	#define _GETCH_ getch
+#endif
+
+//change this to expand to (a) to see list debug messages.
+//Will also pretty much make the output unreadable
+#define LISTDEBUG(a)
+
+/*
+
+MIMA Simulator
+Input file needs to be ASCII-Format Memory
+Map. See mimasm http:/dev.cbcdn.com/kit/mimasm/
+
+06062012 ~1200 Project begin
+06062012 1734 Basic functionality pass
+06062012 1924 Extended functionality pass
+07062012 1632 Fixed some bugs
+07062012 1750 Reworked output calls
+08062012 1652 Implemented file output format, interactive 'm'
+09062012 1435 Shortened setcell by a considerable amount. yay me.
+09062012 1458 Compiles under linux now. Interactive mode not as pretty.
+11062012 1034 Found a replacement for getch. Works just as fine.
+
+FJS 2012
+*/
+
+#define OPCODE(a) (((a)&0xF00000)>>20)
+#define EXTOPCODE(a) (((a)&0xF0000)>>16)
+#define PARAMETER(a) ((a)&0xFFFFF)
+#define MIMAWORD(a) ((a)&0xFFFFFF)
+
+#define ULONG unsigned
+
+typedef struct _MEMCELL {
+	struct _MEMCELL* next;
+	struct _MEMCELL* prev;
+	char* name;
+	ULONG mempos;
+	ULONG value;
+} MEMCELL;
+
+struct /*GLOBAL_OPTS*/ {
+	bool verbose;
+	bool dumpverbose;
+	ULONG maxsteps;
+} OPTIONS;
+
+struct /*MEM*/ {
+	MEMCELL bottom;
+	MEMCELL* top;
+} MEMORY;
+
+struct /*MIMA*/{
+	MEMCELL* ir;
+	ULONG iar;
+	ULONG akku;
+	ULONG steps;
+	bool running;
+} MIMA;
+
+FILE *input=NULL,*output=NULL;
+
+//Linked List Prototypes
+void setcell(ULONG, ULONG); //set cell to value
+MEMCELL* getcell(ULONG); //get cell
+void freemem(); //free list
+
+/*
+Used some kind of special "always-sorted weirdly
+pseudo-optimized" linked list because most programs
+don't need the entire memory to be allocated at any
+point in execution. Also I liked the challenge of
+implementing it.
+*/
+
+void printstate(char*,bool,MEMCELL*); //pretty-print cell name, address and value
+
+void printstate(char* cmd,bool deref,MEMCELL* modified){
+	ULONG param=PARAMETER(MIMA.ir->value);
+	MEMCELL* cell;
+	
+	if(deref){
+		cell=getcell(param);
+	}
+	
+	printf("[%5d] [@0x%06X] ",MIMA.steps,MIMA.ir->mempos);
+	
+	if(MIMA.ir->name&&OPTIONS.verbose){
+		printf("[%s] ",MIMA.ir->name);
+	}
+	else{
+		printf("\t");
+	}
+	
+	printf(cmd);
+	if(output){
+		//steps & label
+		fprintf(output," %8d  %10s ",MIMA.steps,(OPTIONS.verbose&&MIMA.ir->name)?MIMA.ir->name:"");
+		//ir & op
+		fprintf(output," 0x%06X  %6s ",MIMA.ir->value,cmd);
+		
+		if(deref&&(!OPTIONS.verbose||(OPTIONS.verbose&&cell->name==NULL))||(!deref&&modified==NULL)){
+			fprintf(output,"   0x%06X ",param);
+		}
+		else{
+			fprintf(output," %10s ",(deref&&OPTIONS.verbose&&cell->name!=NULL)?cell->name:"");
+		}
+		
+		fprintf(output," 0x%06X  0x%06X ",MIMA.akku,MIMA.iar);
+		
+		if(modified&&deref){
+			if(modified->name&&OPTIONS.verbose){
+				fprintf(output," %s",modified->name);
+			}
+			else{
+				fprintf(output," 0x%06X",modified->mempos);
+			}
+			fprintf(output," = 0x%06X",modified->value);
+		}
+		fputs("\r\n",output);
+	}
+	
+	if(!deref){
+		if(modified!=NULL){
+			printf("\n");
+		}
+		else{
+			printf("0x%06X\n",param);
+		}
+	}
+	else if(OPTIONS.verbose&&cell->name){
+		printf("%s (0x%06X)\n",cell->name,cell->value);
+	}
+	else{
+		printf("0x%06X(@0x%06X)\n",cell->mempos,cell->value);
+	}
+}
+
+MEMCELL* getcell(ULONG pos){
+	LISTDEBUG(printf(" GET 0x%X ",pos));
+	if(pos>MEMORY.top->mempos){
+		LISTDEBUG(printf("INS TOP %d@0x0\n",pos));
+		//create at top
+		MEMCELL* new=(MEMCELL*)malloc(sizeof(MEMCELL));
+		new->mempos=pos;
+		new->name=NULL;
+		new->value=0;
+		
+		//mount
+		new->next=NULL;
+		new->prev=MEMORY.top;
+		MEMORY.top->next=new;
+		MEMORY.top=new;
+		return new;
+	}
+	
+	bool forward=true;
+	MEMCELL* it=&MEMORY.bottom;
+	
+	ULONG delta=MEMORY.top->mempos-pos;
+	if(delta<(MEMORY.top->mempos/2)){
+		//start from top
+		forward=false;
+		it=MEMORY.top;
+		LISTDEBUG(printf("TRAV top | "));
+	}
+	else{
+		LISTDEBUG(printf("TRAV bot | "));
+	}
+	
+	while(it!=NULL){
+		
+		if(it->mempos==pos){
+			LISTDEBUG(printf("FOUND %d@0x%X\n",it->mempos,it->value));
+			return it;
+		}
+		
+		if(forward&&it->next!=NULL&&it->next->mempos>pos||!forward&&it->prev!=NULL&&it->prev->mempos<pos){
+			//insert
+			MEMCELL* new=(MEMCELL*)malloc(sizeof(MEMCELL));
+			new->name=NULL;
+			new->mempos=pos;
+			new->value=0;
+		
+			//mount
+			new->next=forward?(it->next):(it);
+			new->prev=forward?(it):(it->prev);
+			if(forward){
+				it->next->prev=new;
+				it->next=new;
+			}
+			else{
+				it->prev->next=new;
+				it->prev=new;
+			}
+			LISTDEBUG(printf("INS %d@0x0 OK\n",pos));
+			return it;
+		}
+		
+		it=forward?(it->next):(it->prev);
+	}
+	LISTDEBUG(printf("FAIL\n"));
+	return NULL;
+}
+
+void setcell(ULONG pos, ULONG value){
+	//just realized, this is a one-liner. ohwell, overengineering ftw. //09062012 1434
+	getcell(pos)->value=value;
+}
+
+void freemem(){
+	//FIXME the output in here could be done nicer
+	MEMCELL* current=MEMORY.bottom.next;
+	if(OPTIONS.dumpverbose||MEMORY.bottom.name){
+		printf(" END: Cell 0x00000 %s%s%swas at 0x%X\n",(MEMORY.bottom.name)?"(":"",(MEMORY.bottom.name)?MEMORY.bottom.name:"",(MEMORY.bottom.name)?") ":"",MEMORY.bottom.value);
+	}
+	while(current!=NULL){
+		MEMCELL* temp=current;
+		
+		if(OPTIONS.dumpverbose||temp->name){
+			printf(" END: Cell 0x%05X %s%s%swas at 0x%X\n",temp->mempos,(temp->name)?"(":"",(temp->name)?temp->name:"",(temp->name)?") ":"",temp->value);
+		}
+		
+		if(current->name){
+			free(current->name);
+		}
+		
+		current=current->next;		
+		free((void*)temp);//FIXME wat
+	}
+	return;
+}
+
+int main(int argc, char* argv[]){
+	//init memory
+	MEMORY.top=&MEMORY.bottom;
+	MEMORY.bottom.next=NULL;
+	MEMORY.bottom.prev=NULL;
+	MEMORY.bottom.mempos=0;
+	MEMORY.bottom.value=0;
+	MEMORY.bottom.name=NULL;
+
+	if(argc<2){
+		printf("Usage: mimasim [-i] [-dv] [-v] [-e <entrypoint>] [-l <maxsteps>] <infile> [<outfile>]\n");
+		printf("\t-i\tInteractive execution\n");
+		printf("\t-v\tVerbose mode (resolve labels)\n");
+		printf("\t-dv\tverbose memory dump (all active cells)\n");
+		return 0;
+	}
+	
+	char* infile=NULL;
+	char* outfile=NULL;
+	bool interact=false;
+	OPTIONS.verbose=false;
+	OPTIONS.dumpverbose=false;
+	OPTIONS.maxsteps=0;
+	ULONG entry=0;
+	
+	//parse arguments
+	for(int a=1;a<argc;a++){
+		if(*argv[a]=='-'){
+			if(argv[a][1]=='i'||argv[a][1]=='I'){
+				printf("Using interactive mode\n");
+				interact=true;
+			}
+			else if(argv[a][1]=='d'||argv[a][1]=='D'){
+				if(argv[a][2]=='v'||argv[a][2]=='V'){
+					printf("Setting verbose dump flag\n");
+					OPTIONS.dumpverbose=true;
+				}
+			}
+			else if(argv[a][1]=='v'||argv[a][1]=='V'){
+				printf("Increased verbosity\n");
+				OPTIONS.verbose=true;
+			}
+			else if((argv[a][1]=='e'||argv[a][1]=='E')){
+				if(argc>a+1){
+					entry=strtoul(argv[++a],NULL,0);
+					printf("Using entry point 0x%06X\n",entry);
+				}
+				else{
+					printf("Entry point flag was specified, but had no argument.\n");
+					return 1;
+				}
+			}
+			else if((argv[a][1]=='l'||argv[a][1]=='L')){
+			if(argc>a+1){
+					OPTIONS.maxsteps=strtoul(argv[++a],NULL,0);
+					printf("Doing %d steps at max\n",OPTIONS.maxsteps);
+				}
+				else{
+					printf("Limit steps flag was specified, but had no argument.\n");
+					return 1;
+				}
+			}
+			else{
+				printf("Unknown flag \"%s\"\n",argv[a]);
+			}
+		}
+		else{
+			if(!infile){
+				printf("Using input %s\n",argv[a]);
+				infile=argv[a];
+			}
+			else if(!outfile){
+				printf("Using output %s\n",argv[a]);
+				outfile=argv[a];
+			}
+			else{
+				printf("Could not assign parameter \"%s\"\n",argv[a]);
+			}
+		}
+	}
+	
+	//read input file
+	ULONG filesize;
+	char* currentPos;
+	char* inputBuffer;
+	
+	input=fopen(infile,"rb");
+	if(!input){
+		printf("Could not open input file\n");
+		exit(1);
+	}
+
+	if(outfile){
+		output=fopen(outfile,"wb");
+		if(!output){
+			fclose(input);
+			printf("Could not open output file\n");
+			return 1;
+		}
+	}
+	
+	if(outfile){
+		fputs("Reading memory from ",output);
+		fputs(infile,output);
+		fputs("... ",output);
+	}
+	
+	fseek(input,0,SEEK_END);
+	filesize=ftell(input);
+	rewind(input);
+	
+	inputBuffer=(char*)malloc(filesize+1);
+	fread(inputBuffer,1,filesize,input);
+	inputBuffer[filesize]=0;//terminate for security
+	currentPos=inputBuffer;
+	
+	while(currentPos<inputBuffer+filesize-2){
+		ULONG address=strtoul(currentPos,&currentPos,0);
+		ULONG value=strtoul(currentPos,&currentPos,0);
+		//skip leading
+		for(;*currentPos!=';'&&*currentPos!='\n'&&*currentPos!=0;currentPos++){
+		}
+		
+		setcell(address,value);
+		
+		if(*currentPos==';'){
+			currentPos++;
+			ULONG clen=0;
+			for(;currentPos[clen]!=0&&currentPos[clen]!='\n';clen++){
+			}
+			//read comment
+			//store
+			MEMCELL* cur=getcell(address);
+			cur->name=(char*)malloc(clen+1);
+			strncpy(cur->name,currentPos,clen);
+			cur->name[clen]=0;
+			currentPos+=clen+1;
+		}
+	}
+	
+	if(outfile){
+		fputs(" OK\r\n",output);
+		fprintf(output,"[%8s][%10s][%8s][%6s][%10s][%8s][%8s][%-20s]\r\n","STEP","LABEL","IR   ","OP  ","PARAMETER","AKKU  ","IAR  "," MEM");
+	}
+	
+	char addrbuf[20];
+	
+	//simulation code goes here
+	MIMA.iar=entry;
+	MIMA.running=true;
+	MIMA.steps=1;
+	
+	MEMCELL* temp;
+	ULONG buf=0;
+
+	if(interact){
+		printf("Interactive mode commands:\n");
+		printf("\tq\t\tstop execution\n");
+		printf("\ta\t\tprint accumulator\n");
+		printf("\ti\t\tprint iar\n");
+		printf("\tm\t\tquery memory contents\n");
+		printf("\tn|<retn>\texecute next step\n");
+	}
+	
+	while(MIMA.running){
+		if(interact){
+			bool next=false;
+			bool quit=false;
+			//interactive shell
+			while(!next){
+				printf("> ");
+				switch(_GETCH_()){
+					case 'm':
+					case 'M':
+						printf("Addr ?> 0x");
+						unsigned addr=0;
+						fflush(stdin);
+						scanf("%x",&addr);
+						MEMCELL* temp=getcell(addr);
+						printf("0x%06X @ 0x%06X\n",temp->mempos,temp->value);
+						break;
+				
+					case 'q':
+					case 'Q':
+						printf("\n");
+						next=true;
+						quit=true;
+						break;
+					
+					case 'n':
+					case 'N':
+					case 13:
+					case 10:
+						//printf("\n");
+						next=true;
+						break;
+					
+					case 'a':
+					case 'A':
+						printf("Akku: 0x%06X\n",MIMA.akku);
+						break;
+						
+					case 'i':
+					case 'I':
+						printf(" IAR: 0x%06X\n",MIMA.iar);
+						break;
+						
+					default:
+						printf("Unrecognized input\n");
+						break;
+				}
+			}
+			
+			if(quit){
+				break;
+			}
+		}
+		
+		MIMA.ir=getcell(MIMA.iar++);
+		switch(OPCODE(MIMA.ir->value)){
+			case 0x0://LDC
+				MIMA.akku=PARAMETER(MIMA.ir->value);
+				printstate("LDC ",false,NULL);
+				break;
+			case 0x1://LDV
+				temp=getcell(PARAMETER(MIMA.ir->value));
+				MIMA.akku=temp->value;
+				printstate("LDV ",true,NULL);
+				break;
+			case 0x2://STV
+				setcell(PARAMETER(MIMA.ir->value),MIMA.akku);
+				printstate("STV ",true,getcell(PARAMETER(MIMA.ir->value)));
+				break;
+			case 0x3://ADD
+				temp=getcell(PARAMETER(MIMA.ir->value));
+				MIMA.akku=MIMAWORD(MIMA.akku+temp->value);
+				printstate("ADD ",true,NULL);
+				break;
+			case 0x4://AND
+				temp=getcell(PARAMETER(MIMA.ir->value));
+				MIMA.akku&=temp->value;
+				printstate("AND ",true,NULL);
+				break;
+			case 0x5://OR
+				temp=getcell(PARAMETER(MIMA.ir->value));
+				MIMA.akku|=temp->value;
+				printstate("OR ",true,NULL);
+				break;
+			case 0x6://XOR
+				temp=getcell(PARAMETER(MIMA.ir->value));
+				MIMA.akku^=temp->value;
+				printstate("XOR ",true,NULL);
+				break;
+			case 0x7://EQL
+				temp=getcell(PARAMETER(MIMA.ir->value));
+				MIMA.akku=(MIMA.akku==temp->value)?MIMAWORD(-1):0;
+				printstate("EQL ",true,NULL);
+				break;
+			case 0x8://JMP
+				MIMA.iar=PARAMETER(MIMA.ir->value);
+				printstate("JMP ",true,NULL);
+				break;
+			case 0x9://JMN
+				if((MIMA.akku&0x800000)!=0){
+					MIMA.iar=PARAMETER(MIMA.ir->value);
+				}
+				printstate("JMN ",true,NULL);
+				break;
+			case 0xA://LDIV
+				temp=getcell(PARAMETER(MIMA.ir->value));
+				MIMA.akku=getcell(temp->value)->value;
+				printstate("LDIV ",true,NULL);
+				break;
+			case 0xB://STIV
+				temp=getcell(PARAMETER(MIMA.ir->value));
+				setcell(temp->value,MIMA.akku);
+				printstate("STIV ",true,getcell(temp->value));
+				break;
+			case 0xC://JMS
+				setcell(PARAMETER(MIMA.ir->value),MIMA.iar);
+				MIMA.iar=PARAMETER(MIMA.ir->value)+1;
+				printstate("JMS ",true,getcell(PARAMETER(MIMA.ir->value)));
+				break;
+			case 0xD://JIND
+				temp=getcell(PARAMETER(MIMA.ir->value));
+				MIMA.iar=temp->value;
+				printstate("JIND ",true,NULL);
+				break;
+			case 0xF:
+				switch(EXTOPCODE(MIMA.ir->value)){
+					case 0x0://HALT
+						MIMA.running=false;
+						printstate("HALT ",false,(void*)!NULL);//FIXME ugly
+						break;
+					case 0x1://NOT
+						MIMA.akku=MIMAWORD(~MIMA.akku);
+						printstate("NOT ",false,(void*)!NULL);
+						break;
+					case 0x2://RAR
+						buf=MIMA.akku&1;
+						MIMA.akku>>=1;
+						buf<<=23;
+						MIMA.akku|=buf;
+						printstate("RAR ",false,(void*)!NULL);
+						break;
+					default://FAIL
+						printf("OP %X, EXOP %X ",OPCODE(MIMA.ir->value),EXTOPCODE(MIMA.ir->value));
+						printstate("FAIL ",false,(void*)!NULL);
+						break;
+				}
+				break;
+		}
+		if(MIMA.steps==OPTIONS.maxsteps){//wont fire if 0
+			printf("> Reached allowed step limit, aborting\n");
+			break;
+		}
+		MIMA.steps++;
+	}
+	
+	if(interact){
+		printf("> Execution stopped\n");
+		_GETCH_();
+	}
+	
+	freemem();
+	fclose(input);
+	free(inputBuffer);
+	return 0;
+}
---
+++ mimasm/mimasm.c
@@ -0,0 +1,634 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <malloc.h>
+#include <ctype.h>
+
+/**
+04062012 1545 File creation
+04062012 2200 Functionality pass
+06062012 1955 Pretty much complete
+
+Known Bugs
+----------
+GCC Compilation outputs weird stuff on stdout //FIXED 05062012 1132
+HALT on last line not recognized //FIXED 05062012 1050
+Preprocessor Constants dont work as expected //FIXED 05062012 1413
+Initialize empty DS' to 0 //FIXED 05062012 1143
+ORG's dont work properly without spaces //FIXED 05062012 2057
+
+Notes
+----------
+Added JMS + JIND 06062012 1909
+Fixed subtle bug with files not having a newline at the end 06062012 2231
+
+About
+----------
+Had this idea and just had to implement it.
+Reads a file of MIMA Assembler and creates an ASCII Memory
+Map to be read by an interpreter. Should be pretty fault
+tolerant, wont do anything flashy on encountering an error tho.
+Might want to watch stdout closely for that.
+
+If you're looking for an interpreter, I wrote one.
+Check http://dev.cbcdn.com/kit/mimasim/
+
+FJS 2012
+**/
+
+#define ULONG unsigned long
+#define MIMAWORD(a) ((a)&0xFFFFFF)
+#define MIMAPARAM(a) ((a)&0xFFFFF)
+
+#define STATE_WARN 1
+#define STATE_ERR 2
+
+typedef struct /*GLOBTABLE*/ {
+	char* name;
+	ULONG value;
+} GLOBAL;
+
+GLOBAL** globals=NULL;
+
+//global table handling prototypes
+ULONG globalcreate(char*, ULONG, ULONG);//create global from buffer, namelength, value
+bool globalexists(char*, ULONG);//query global existence with name from buffer
+ULONG globalvalue(char*, ULONG);//query global value with name from buffer
+void freeglobals();//free global table entries
+
+//parsing prototypes
+ULONG parseint(char*,ULONG,ULONG);//parse integral values (hex/dec)
+ULONG getparam(char*,ULONG);//get parameter from sentence
+ULONG getopcode(char*, ULONG);//get opcode from sentence
+ULONG label(char*, ULONG);//get label length or 0 if none
+
+//auxiliary prototypes
+void printlen(char*, ULONG, ULONG);
+
+void printlen(char* mem, ULONG start, ULONG len){
+	if(len==0){
+		//print until line end
+		for(;mem[start]!=0&&mem[start]!='\n';start++){
+			putc(mem[start],stdout);
+		}
+	}
+	else{
+		//print length
+		for(ULONG i=0;i<len;i++){
+			putc(mem[start+i],stdout);
+		}
+	}
+}
+
+ULONG globalvalue(char* mem, ULONG start){
+	if(!globals){
+		printf(" [G:EMPTY] ");
+		return 0;
+	}
+	
+	ULONG llen=0;
+	bool end=false;
+	while(!end){
+		switch(mem[start+llen]){
+			case '\n':
+			case ' ':
+			case ':':
+			case '=':
+			case 0:
+				end=true;
+				break;
+			default:
+				llen++;
+		}
+	}
+	
+	for(int i=0;globals[i];i++){
+		if(llen==strlen(globals[i]->name)&&!strncmp(mem+start,globals[i]->name,llen)){
+			return globals[i]->value;
+		}
+	}
+	printf(" [G:NOSUCH:");
+	printlen(mem,start,llen);
+	printf("]");
+	
+	return 0xDEAD;
+}
+
+bool globalexists(char* mem, ULONG start){
+	if(!globals){
+		printf("[G:EMPTY] ");
+		return false;
+	}
+	
+	ULONG llen=0;
+	bool end=false;
+	while(!end){
+		switch(mem[start+llen]){
+			case ' ':
+			case ':':
+			case '\n':
+			case '=':
+			case 0:
+				end=true;
+				break;
+			default:
+				llen++;
+		}
+	}
+	
+	//printf("[LEN%d]",llen);
+	
+	for(int i=0;globals[i];i++){
+		if(strlen(globals[i]->name)==llen&&!strncmp(mem+start,globals[i]->name,llen)){
+			//printf("[G:FOUND] ");
+			return true;
+		}
+	}
+	
+	//printf("[G:NOEXST:");
+	//printlen(mem,start,llen);
+	//printf("] ");
+	
+	return false;
+}
+
+void freeglobals(){
+	if(!globals){
+		return;
+	}
+	
+	for(int i=0;globals[i]!=NULL;i++){
+		printf(" G:CLEAN: Freeing %10s @ %6X\n",globals[i]->name,globals[i]->value);
+		free(globals[i]->name);
+		free(globals[i]);
+	}
+	free(globals);
+}
+
+ULONG globalcreate(char* mem, ULONG start, ULONG value){
+
+	ULONG length=label(mem,start);
+	if(length<1){
+		printf(" [G:ERR: NO LABEL] ");
+		return STATE_ERR;
+	}
+	
+	//printf(" [G:LENGTH: %d] ",length);
+	
+	if(!globals){//lazy init
+		globals=(GLOBAL**)malloc(sizeof(GLOBAL*));
+		if(!globals){
+			printf("M:CRITICAL: ALLOCATION FAILED @ %d",__LINE__);
+			exit(1);
+		}
+		globals[0]=NULL;
+	}
+	
+	if(globalexists(mem,start)){
+		printf("!G:WARN: ");
+		printlen(mem,start,length-1);
+		printf(" EXISTS");
+		return STATE_WARN;
+	}
+	
+	ULONG num_globs=0;
+	for(;globals[num_globs]!=NULL;num_globs++){
+	}
+	
+	globals=(GLOBAL**)realloc(globals,sizeof(GLOBAL*)*(num_globs+2));
+	if(!globals){
+		printf("M:CRITICAL: ALLOCATION FAILED @ %d",__LINE__);
+		exit(1);
+	}
+	globals[num_globs]=(GLOBAL*)malloc(sizeof(GLOBAL));
+	if(!globals[num_globs]){
+		printf("M:CRITICAL: ALLOCATION FAILED @ %d",__LINE__);
+		exit(1);
+	}
+	globals[num_globs+1]=NULL;
+	
+	globals[num_globs]->value=value;
+	globals[num_globs]->name=malloc((length+1)*sizeof(char));
+	if(!globals[num_globs]->name){
+		printf("M:CRITICAL: ALLOCATION FAILED @ %d",__LINE__);
+		exit(1);
+	}
+	strncpy(globals[num_globs]->name,mem+start,length-1);
+	
+	globals[num_globs]->name[length-1]=0; //ensure proper termination. fixes GCC output issues
+	//trim trailing spaces
+	for(ULONG i=strlen(globals[num_globs]->name)-1;globals[num_globs]->name[i]==' ';i--){
+		globals[num_globs]->name[i]=0;
+	}
+	printf(" G:STORE: %s=0x%X",globals[num_globs]->name,globals[num_globs]->value);
+}
+
+ULONG getparam(char* mem,ULONG start){
+	//skip blanks
+	for(;mem[start]==' ';start++){
+	}
+	//find next blank (skip op)
+	for(;mem[start]!=' ';start++){
+		if(mem[start]==0||mem[start]=='\n'){
+			return 0;
+		}
+	}
+	start++;
+	
+	ULONG end=start;
+	for(;mem[end]!='\n'&&mem[end]!=0;end++){
+	}
+	
+	if(mem[start]=='$'||isdigit(mem[start])){
+		return parseint(mem,start,end);
+	}
+	
+	if(globalexists(mem,start)){
+		return(globalvalue(mem,start));
+	}
+	
+	printf("[P:ERR: NOPARAM] ");
+	
+	return 0;
+}
+
+
+ULONG getopcode(char* mem, ULONG start){
+	//skip blank
+	for(;mem[start]==' ';start++){
+	}
+	if(mem[start]==0||mem[start]=='\n'){
+		return 0;
+	}
+	
+	int len=0;
+	for(;mem[start+len]!=' '&&mem[start+len]!=0&&mem[start+len]!='\n';len++){
+	}
+	
+	//printf(" [OPLEN %d] ",len);
+	if(len>2){
+		if(!strncmp(mem+start,"LD",2)){
+			switch(mem[start+2]){
+				case 'C'://LDC
+					return 0;
+				case 'V'://LDV
+					return 1;
+				case 'I'://LDIV
+					return 0xA;
+			}
+		}
+		
+		if(!strncmp(mem+start,"ST",2)){
+			switch(mem[start+2]){
+				case 'V'://STV
+					return 2;
+				case 'I'://STIV
+					return 0xB;
+			}
+		}
+		
+		if(mem[start]=='A'){
+			switch(mem[start+1]){
+				case 'D'://ADD
+					return 3;
+				case 'N'://AND
+					return 4;
+			}
+		}
+		
+		if(mem[start]=='O'){//OR
+			return 5;
+		}
+		
+		if(mem[start]=='X'){//XOR
+			return 6;
+		}
+		
+		if(mem[start]=='E'){//EQL
+			return 7;
+		}
+		
+		if(!strncmp(mem+start,"JI",2)){
+			return 13;
+		}
+		
+		if(!strncmp(mem+start,"JM",2)){
+			switch(mem[start+2]){
+				case 'P'://JMP
+					return 8;
+				case 'N'://JMN
+					return 9;
+				case 'S'://JMS
+					return 12;
+			}
+		}
+		
+		if(mem[start]=='H'&&len>3){//HALT
+			return 0xF0;
+		}
+		
+		if(mem[start]=='N'){//NOT
+			return 0xF1;
+		}
+		
+		if(mem[start]=='R'){//RAR
+			return 0xF2;
+		}
+	}
+	
+	printf(" [NOP %d, %c] ",len,mem[start]);	
+	return 0;
+}
+ 
+ULONG parseint(char* mem,ULONG start,ULONG end){
+	int base=10;
+	bool negative=false;
+	for(;start<end;start++){
+		switch(mem[start]){
+			case '$':
+				base=16;
+				break;
+			case 0:
+			case '\n':
+				return 0;
+				break;
+			case ' ':
+				break;
+			default:
+				return strtol(mem+start,NULL,base);
+				break;
+		}
+	}
+	return 0;
+}
+
+ULONG label(char* mem, ULONG start){
+	ULONG _start=start;
+	for(;mem[start]!=0;start++){
+		switch(mem[start]){
+			case '\n':
+				return 0;
+			case ':':
+			case '=':
+				return start-_start+1;
+			default:
+				break;
+		}
+	}
+	return 0;
+}
+
+int main(int argc, char* argv[]){
+	if(argc<2){
+		printf("No input file. Usage: mimasm <infile> [<outfile>]\n");
+		printf("Standard output file is out.mima");
+		return 0;
+	}
+	
+	printf("M:INIT: Opening %s for mode rb\n",argv[1]);
+	
+	FILE *input,*output;
+	long filesize;
+	ULONG linecount=1;
+	char* buffer=NULL;
+	ULONG bufferpointer=0,buflen=0;
+	int inBuf;
+	char* outfile="out.mima";
+	bool comment=false,hadspace=false,hadnewline=false;
+	
+	if(argc>2){
+		outfile=argv[2];
+	}
+	
+	input=fopen(argv[1],"rb");
+	if(!input){
+		printf("M:ERR: Could not open input file\n");
+		return 1;
+	}
+	
+	output=fopen(outfile,"wb");
+	if(!output){
+		fclose(input);
+		printf("M:ERR: Could not open output file\n");
+		return 1;
+	}
+	
+	fseek(input,0,SEEK_END);
+	filesize=ftell(input);
+	rewind(input);
+	
+	printf("M:INFO: File is %d bytes long\n",filesize);
+
+	buffer=malloc(filesize+1);
+	
+	while(!feof(input)){
+		inBuf=fgetc(input);
+		switch(inBuf){
+			case ';':
+				comment=true;
+				break;
+			case ' ':
+			case '\t':
+				if(!hadspace&&!comment&&(buflen>1&&*(buffer+buflen-1)!='\n')){
+					*(buffer+buflen++)=' ';
+				}
+				hadspace=true;
+			case -1://fixes missing newline bug
+			case '\r':
+				break;
+			case '\n':
+				//if(!hadnewline&&(buflen>1&&*(buffer+buflen-1)!='\n')){
+					*(buffer+buflen++)='\n';
+					linecount++;
+				//}
+				hadnewline=true;
+				comment=false;
+				break;
+			
+			default:
+				hadnewline=false;
+				hadspace=false;
+				if(!comment){
+					*(buffer+buflen++)=inBuf;
+				}
+				break;
+		}
+		*(buffer+buflen+1)=0;
+	}	
+	printf("M:INFO: Read %d sentences\n",linecount);
+	
+	ULONG i=0;
+	ULONG currentmem=0;
+	ULONG warns=0;
+	char memstring[20],bufstring[20];
+	
+	printf("M:INFO: Building global table...\n");
+	while(i<buflen-1){
+		ULONG sent_start=i;
+		ULONG sent_end=i;
+		for(;sent_end<buflen-1;sent_end++){
+			if(buffer[sent_end]=='\n'){
+				break;
+			}
+		}
+		
+		ULONG slen=sent_end-sent_start;
+		
+		//detect directives
+			if(buffer[sent_start]=='*'){
+				if(slen>=3){
+					//to int
+					for(;buffer[sent_start]!='=';sent_start++){
+					}
+					currentmem=parseint(buffer,sent_start+1,sent_end);
+					printf(" M:INFO: ORG to 0x%X\n",currentmem);
+					currentmem--;
+				}
+			}
+			else{
+				ULONG labelpos=label(buffer,sent_start);
+				if(labelpos>0){
+					printf(" [%c]",buffer[sent_start+labelpos-1]);
+					if(buffer[sent_start+labelpos-1]=='='){
+						switch(globalcreate(buffer,sent_start,parseint(buffer,sent_start+labelpos,sent_end))){
+							case STATE_WARN:
+								warns++;
+								break;
+							case STATE_ERR:
+								break;
+						}
+						currentmem--;
+					}
+					else{
+						switch(globalcreate(buffer,sent_start,currentmem)){
+							case STATE_WARN:
+								warns++;
+								break;
+							case STATE_ERR:
+								break;
+						}
+					}
+					printf("\n");
+				}
+			}
+
+		currentmem++;
+		i=sent_end+1;
+	}
+	printf("M:INFO: Global Table built with %d warnings\n",warns);
+	
+	currentmem=0;
+	i=0;
+	//parse
+	
+	printf("M:INFO: Parsing\n");
+	while(i<buflen-1){
+		sprintf(memstring,"0x%06X ",currentmem);
+		ULONG sent_start=i;
+		ULONG sent_end=i;
+		for(;sent_end<buflen-1;sent_end++){
+			if(buffer[sent_end]=='\n'||buffer[sent_end]==0){
+				break;
+			}
+		}
+		
+		printf(" P:LINE: %d - %d: ",sent_start,sent_end);
+		ULONG slen=sent_end-sent_start;
+		
+		//parse here
+			//detect orgs & metas
+			if(buffer[sent_start]=='*'){
+				if(slen>=3){
+					//to int
+					for(;buffer[sent_start]!='=';sent_start++){
+					}
+					currentmem=parseint(buffer,sent_start+1,sent_end);
+					printf("ORG to 0x%X\n",currentmem);
+					currentmem--;
+				}
+			}
+			//detect blanks => LDC 0
+			else if(sent_start==sent_end){
+				fputs(memstring,output);
+				fputs("0x000000\n",output);
+				printf("BLANK -> LDC 0\n");
+			}
+			
+			else{
+				//handle labelling
+				ULONG labelpos=label(buffer,sent_start);
+				if(labelpos>0){
+					if(buffer[sent_start+labelpos-1]=='='){
+						//ignore line
+						printf("SKIP [META]\n");
+						i=sent_end+1;
+						continue;
+					}
+					else{
+						printf("[LABEL@+%d] ",labelpos);
+					}
+				}
+				
+				sent_start+=labelpos;
+				sent_start+=(buffer[sent_start]==' ')?1:0;
+				
+				//TODO if now blank, ldc 0 //should work
+	
+				if(buffer[sent_start]=='D'&&buffer[sent_start+1]=='S'){
+					//handle DS
+					ULONG storage=parseint(buffer,sent_start+2,sent_end);
+					fputs(memstring,output);
+					sprintf(bufstring,"0x%06X",MIMAWORD(storage));
+					fputs(bufstring,output);
+					if(labelpos>0){
+						fputs(" ;",output);
+						fwrite(buffer+(sent_end-slen),labelpos-1,1,output);
+					}
+					fputc('\n',output);
+					printf("DS 0x%06X\n",MIMAWORD(storage));
+				}
+				else{
+					//parse opcode
+					ULONG opcode=getopcode(buffer,sent_start);
+					
+					printf("OP 0x%X ",opcode);
+					fputs(memstring,output);
+					
+					if(opcode<=0xF){
+						//parse parameter
+						ULONG parameter=getparam(buffer,sent_start);
+						
+						printf("PARAM 0x%05X",MIMAPARAM(parameter));
+						sprintf(bufstring,"0x%X%05X",opcode,MIMAPARAM(parameter));
+						fputs(bufstring,output);
+					}
+					else{
+						sprintf(bufstring,"0x%06X",(opcode<<16));
+						fputs(bufstring,output);
+					}
+					
+					printf(" <= ");		
+					printlen(buffer,sent_start,sent_end-sent_start+(buffer[sent_end]=='\n'?0:1));//weird construct, but fixes an output bug
+					
+					if(labelpos>0){
+						fputs(" ;",output);
+						fwrite(buffer+(sent_end-slen),labelpos-1,1,output);
+					}
+					fputc('\n',output);
+					putc('\n',stdout);
+				}
+			}
+			
+		i=sent_end+1;
+		currentmem++;
+	}
+	printf("M:INFO: Parser done\n");
+	printf("M:INFO: Cleaning up\n");
+	
+	
+	freeglobals();
+	free(buffer);
+	fclose(input);
+	fclose(output);
+}
